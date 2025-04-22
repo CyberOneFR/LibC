bc165ccc89958fbe06366dc9293c9ec08e3bc8f4
CyberOneFR <erwoan.thebault@gmail.com>
2025-04-22 23:45:07 +0200

    updated

--- srcs/string/ft_strjoin.c
+++ srcs/string/ft_strjoin.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strjoin.c                                          :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ethebaul <ethebaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 18:27:59 by ethebaul          #+#    #+#             */
-/*   Updated: 2025/04/22 18:52:17 by ethebaul         ###   ########.fr       */
+/*   Updated: 2025/04/22 23:40:59 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strjoin(t_array *tab, t_string *ret)
 	t_string	*str;
 	uint		i;
 
-	ret->size = ft_sum_array(tab, strsize);
+	ret->size = ft_sum_array(tab, ft_strsize);
 	ret->content = (char *) malloc(ret->size * sizeof(char));
 	if (!ret->content)
 		return (1);
--- srcs/string/ft_strsplit.c
+++ srcs/string/ft_strsplit.c
@@ -1,12 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strsplit.c                                         :+:      :+:    :+:   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ethebaul <ethebaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 18:28:02 by ethebaul          #+#    #+#             */
-/*   Updated: 2025/04/21 18:28:02 by ethebaul         ###   ########.fr       */
+/*   Updated: 2025/04/22 23:41:13 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libc.h"
+
+t_array	*ft_strsplit(t_string *str, char sep)
+{
+	uint	i;
+	uint	count;
+
+	i = 0;
+	count = 0;
+	while (i < str->size)
+	{
+		if (str->content[i] != sep)
+			++count;
+		++i;
+	}
+}
